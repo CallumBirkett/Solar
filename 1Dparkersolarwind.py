5f2cf2d98cd1c243e863d67ab6a3e47fb82ed1e2
Callum Birkett <callumbirkett@outlook.com>
2025-11-07 21:58:48 +0000

    Added constants file

---
+++ 1Dparkersolarwind.py
@@ -0,0 +1,24 @@
+# AIM: Develop a model for the 1D (radially symmetric Parker Solar Wind) with isothermal flow.
+
+# Mass conservation
+
+# Euler Equations
+
+# Isothermal Closure
+
+# Non-dimensionalize units
+
+# Derive a critical point
+
+# Solve time-dependent equations until steady-state is reached.
+
+# Model validation
+
+"""
+Extensions:
+
+- Polytropic equation of state.
+- Add heating terms.
+- Add rotation.
+- Move to 1D time-dependent HD solver.
+"""
---
+++ constants.py
@@ -0,0 +1,27 @@
+# Fundamental constants
+
+G = 6.67430e-11  # Graviational constant - [m^3 kg^-1 s^-2]
+K_B = 1.380649e-23  # Boltzmann constant - [J K^-1]
+MASS_PROTON = 0.6726219e-27  # Proton mass - [kg]
+
+
+# Solar constants
+MASS_SUN = 1.98847e30  # Solar mass - [kg]
+RADIUS_SUN = 6.9634e8  # Solar radius - [km]
+
+# Plasma constants
+MMW = 0.6  # # Mean Molecular Weight - dimensionless
+TEMP_CORONA = 1.5e6  # Coronal Temperature - [K]
+
+# Optional useful distances
+AU = 1.496e11  # astronomical unit [m]
+
+# Calculated constants
+
+
+def sound_speed(T=TEMP_CORONA, mmw=MMW):
+    """
+    Sound speed is how strongly pressure responds to compression.
+    Derived from the deivative of pressure wrt density.
+    """
+    return (K_B * T / (mmw * MASS_PROTON)) ** 0.5
