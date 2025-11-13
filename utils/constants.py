fb64d6dd6732b56b70b681cac6336270adae967e
Callum Birkett <callumbirkett@outlook.com>
2025-11-13 11:23:57 +0000

    Updated incorrect proton mass

--- utils/constants.py
+++ utils/constants.py
@@ -2,7 +2,7 @@
 
 G = 6.67430e-11  # Graviational constant - [m^3 kg^-1 s^-2]
 K_B = 1.380649e-23  # Boltzmann constant - [J K^-1]
-MASS_PROTON = 0.6726219e-27  # Proton mass - [kg]
+MASS_PROTON = 1.6726219e-27  # Proton mass - [kg]
 
 
 # Solar constants
@@ -15,12 +15,3 @@ TEMP_CORONA = 1.5e6  # Coronal Temperature - [K]
 
 # Optional useful distances
 AU = 1.496e11  # astronomical unit [m]
-
-
-# Calculated constants
-def sound_speed(T=TEMP_CORONA, mmw=MMW):
-    """
-    Sound speed is how strongly pressure responds to compression.
-    Derived from the deivative of pressure wrt density.
-    """
-    return (K_B * T / (mmw * MASS_PROTON)) ** 0.5
