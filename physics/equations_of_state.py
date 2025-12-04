50f24e2173c6cd4e5918a12703688e1965d312d5
CallumBirkett <63651513+CallumBirkett@users.noreply.github.com>
2025-12-04 12:46:19 +0000

    PolytropicEOS added

--- physics/equations_of_state.py
+++ physics/equations_of_state.py
@@ -17,3 +17,32 @@ class IsothermalEOS(EquationOfState):
     def sound_speed(self):
         return np.sqrt(K_B * self.T / (self.mmw * MASS_PROTON))
 
+class PolytropicEOS(EquationOfState):
+    def __init__(self, gamma, K, mmw=MMW):
+        self.gamma = gamma
+        self.K = K 
+        self.mmw = mmw
+
+    @classmethod 
+    def from_base_params(
+        cls, # use a class method for extensibility (e.g. ModifiedPolytropicEOS(PolytropicEOS) produces instances of ModifiedPolytropicEOS)
+        gamma,
+        rho0,
+        T0,
+        mmw=MMW
+    ):
+        P0 = rho0*K_B*T0 / (mmw * MASS_PROTON)
+        K = P0 / (rho0 ** gamma)
+        return cls(gamma=gamma, K=K, mmw=mmw) # return an instance from base parameters
+    
+    def pressure(self, rho):
+        rho = np.asarrary(rho)
+        return self.K * rho ** self.gamma # polytropic pressure
+    
+    def temperature(self, rho):
+        rho = np.asarray(rho)
+        return (self.mmw * MASS_PROTON / K_B) * self.K * self.rho**(self.gamma - 1.0) # P = K * rho ** gamma
+    
+    def sound_speed(self, rho):
+        rho = np.asarray(rho)
+        return np.sqrt(self.gamma * self.K * rho ** (self.gamma - 1.0)) # definition of sound speed
