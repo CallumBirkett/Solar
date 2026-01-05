433a8ed5277ec79dc819206717c31af2cad92c70
CallumBirkett <63651513+CallumBirkett@users.noreply.github.com>
2026-01-05 11:35:36 +0000

    implemented CriticalPoint inteface in isothermal model

--- models/types.py
+++ models/types.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True) # derived values should be immutable
 class CriticalPoint:
-    critical_radius: float
-    critical_speed: float
-    sound_speed_crit: float
-    slope_crit: float 
+    crc: float
+    uc: float
+    cs_crit: float
+    slope: float 
